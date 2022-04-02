ce049c0ee8419be6869a77117ac785ec77e88e9f
Nathan Freeman <NathanLFreeman@gmail.com>
2022-04-01 23:36:43 -0400

    retry on deployment configuration

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(__dirname, '/dist'))
+app.use(express.static(__dirname, '/dist/front-end'))
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'dist/index.html'))
+  res.sendFile(path.join(__dirname + 'dist/front-end/index.html'))
 })
 
-app.listen(process.env.PORT || 4200)
+app.listen(process.env.PORT || 8080)
