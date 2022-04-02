f160a565d42f7f5449713667a1eae555b234e917
Nathan Freeman <NathanLFreeman@gmail.com>
2022-04-01 23:27:14 -0400

    add default path to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 
 const routes: Routes = [
   {path: 'form', component: FormComponent}
+  {path: '', component: AppComponent}
 ];
 
 @NgModule({
